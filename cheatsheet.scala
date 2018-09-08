7ad1245e5dbfa74efcec2a7443e0a0e01828cc03
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-09-08 14:01:59 +0100

    add methods, closure, recursion, block statements

--- cheatsheet.scala
+++ cheatsheet.scala
@@ -26,9 +26,39 @@ variable = 30  // type can't be changed
 // print command
 println(salute)
 
-// object methods.
+// operators
+!true && false || true
+if (true) 1 else 0
+
+// object methods
 "hello".size // no parentheses if no param supplied. Is still method, not prop?
 -20.abs
 
 1.to(10)
 1 to 10  // infix syntax
+
+// method definition
+def triple(x: Double): Double = x * 3  // implicit return
+
+def sumOfTriples(x: Double, y: Double): Double triple(x) + triple(y)
+
+def inferredTriple(x: Int): x * 3  // params are never inferred in defs
+
+List(1,2,3).map(x => x*2)  // params can be inferred in inline anonymous functions
+
+// recursion loop
+def factorial(n: Int): Int = if(n==0) 1 else fac(n-1) * n
+
+factorial(17)  // unguarded overflow
+
+// closures and block expressions
+def doubleAndOne(x: Int): Int = { 
+    def innerDouble(y: Int): Int = y * 2  // scoped definition
+    
+    innerDouble(x) + 1
+}
+
+def doubleWithBlock(x: Int): Int = {
+    val double = { x * x }  // all blocks are expressions
+    double
+}
